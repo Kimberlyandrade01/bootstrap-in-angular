8c377e900e9dcc3932368236c68e84f892967adb
Kimberlyandrade01 <kimberly.andradecervantes@cesunbc.edu.mx>
2025-09-30 08:59:43 -0700

    Agregamos un componente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { TarjetaComponent } from './components/tarjeta/tarjeta.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  
+    {
+        path: 'tarjeta',
+        component: TarjetaComponent
+    }
+    
+];
